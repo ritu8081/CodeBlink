7587f88a09ce5fb0b242214a11b86c3d8410a265
ritu8081 <jadhavritu18@gmail.com>
2024-05-01 23:02:42 +0530

    some basic pages are created --

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css"
-import { Route,Routes } from "react-router-dom";
+// Redux
+// React Router
+import { Route, Routes } from "react-router-dom"
+import "./App.css"
 import Home from "./pages/Home"
 import Navbar from "./components/Common/Navbar";
+import OpenRoute from "./components/core/Auth/OpenRoute"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import ForgotPassword from "./pages/ForgotPassword"
+import UpdatePassword from "./pages/UpdatePassword"
+import VerifyEmail from "./pages/VerifyEmail"
+
 
 function App() {
   return (
@@ -10,7 +20,48 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-    
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+               <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+         <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+         <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
+
+          <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+
     
      </Routes>
 
